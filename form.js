780f0a2d3a1d30aa3d129b92cbb9537d50c2aa1c
Mary P <marylaboitebis@gmail.com>
2023-10-12 11:18:20 +0200

    test 1 -  validation des regex et mise en place du console.log en cas d'erreur

--- form.js
+++ form.js
@@ -5,48 +5,53 @@
 let regexName = new RegExp("[a-z A-Z à-ÿ À-Ý]*");
 let regexMail = new RegExp("^[a-z0-9._-]+@[a-z0-9._-]+.[a-z]{2,20}$");
 let regexBirthday = new RegExp("^(19[0-9]{2}|2[0-9]{3})-(0[1-9]|1[012])-([123]0|[012][1-9]|31)$");
-let regexNumberTournois = new RegExp ("[1-9][0-9]$");
+let regexNumberTournois = new RegExp ("[1-9]*$");
 let valid = true;
+
 //On récupère notre formulaire
 const form = document.querySelector('form');
 let inputDataEnter = form.querySelectorAll(".formData input[data-enter]");
 
 
+// On teste nos valeurs entrées.
 let firstName = regexName.test(inputDataEnter[0].value.trim());
-console.log(firstName);
 let last = regexName.test(inputDataEnter[1].value.trim());
-console.log(last);
 let email = regexMail.test(inputDataEnter[2].value.trim());
-console.log(email);
 let birthday = regexBirthday.test(inputDataEnter[3].value.trim());
-console.log(birthday);
-let tournois = regexBirthday.test(inputDataEnter[4].value.trim());
-console.log(tournois);
-
+let tournois = regexNumberTournois.test(inputDataEnter[4].value.trim());
+ console.log(inputDataEnter[0].id);
 
 // On écoute le submit sur le formulaire
 form.addEventListener("submit", (e) => {
     // On empêche le comportement par défaut pour éviter le rechargement de la page.
     e.preventDefault();
+    
 
-        if(!(firstName)){
-            console.log("Firstname fonctionne aussi !");
+
+        if((!firstName) && (inputDataEnter[0].id)){
+            console.log("Non valide");
         }
 
-        // else if(!(regexName.test(last))){
-        //     console.log("Ca fonctionne ! ");
-    
-        // } 
-        // else if((name==="email") && (!resultatMail)){
-        //     console.log(resultatMail);
-        //     console.log("Non valide !");
+        if(!last){
+         
+        } 
+         if(!email){
+        
+        }
+ 
 
-        else{
+        
+ 
+
+        if(valid){
             valid &= form.reportValidity();
             console.log("Votre message a bien été envoyé");
+            // form.reset();
         }
 
 });
 
-// let test = document.getElementById("firstName").name;
-// console.log(test);
+
+
+
+
