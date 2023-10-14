e103749852d791e08cb7a1b76f8c5f530d494328
Mary P <marylaboitebis@gmail.com>
2023-10-14 23:04:08 +0200

    mise en place de la validation des entrées du formulaire

--- form.js
+++ form.js
@@ -2,56 +2,96 @@
  * Validation du formulaire au submit
  */
 
-let regexName = new RegExp("[a-z A-Z à-ÿ À-Ý]*");
-let regexMail = new RegExp("^[a-z0-9._-]+@[a-z0-9._-]+.[a-z]{2,20}$");
-let regexBirthday = new RegExp("^(19[0-9]{2}|2[0-9]{3})-(0[1-9]|1[012])-([123]0|[012][1-9]|31)$");
-let regexNumberTournois = new RegExp ("[1-9]*$");
-let valid = true;
 
 //On récupère notre formulaire
 const form = document.querySelector('form');
-let inputDataEnter = form.querySelectorAll(".formData input[data-enter]");
+
+// On récupère les données du formulaire
+const inputDataEnter = form.querySelectorAll(".formData input[data-enter]");
+
+//fonction de validation via les regex pour chaque entrée
+function firstNameValidation(e){
+    const error = document.querySelector("div.firstName[data-error]");
+    const firstName = /^[a-z A-Z à-ÿ À-Ý]{2,}$/.test(inputDataEnter[0].value.trim());
+    if(firstName === false){
+        e.preventDefault();
+        error.setAttribute('data-error-visible', 'true');
+        console.log("Le formulaire n'est pas envoyé ! ");
+    }else {
+        error.setAttribute('data-error-visible', 'false');
+        console.log("Le formulaire est envoyé ! ");
+    }
+}   
+
+function showValidation(){
+    
+}
+    
+function lastValidation(e){
+    const error = document.querySelector("div.last[data-error]");
+    const last = /^[a-z A-Z à-ÿ À-Ý]{2,}$/.test(inputDataEnter[1].value.trim());
+    if(last === false){
+        e.preventDefault();
+        error.setAttribute('data-error-visible', 'true');
+        console.log("Le formulaire n'est pas envoyé ! ");
+    }else {
+        error.setAttribute('data-error-visible', 'false');
+        console.log("Le formulaire est envoyé ! ");
+    }
+}   
 
 
-// On teste nos valeurs entrées.
-let firstName = regexName.test(inputDataEnter[0].value.trim());
-let last = regexName.test(inputDataEnter[1].value.trim());
-let email = regexMail.test(inputDataEnter[2].value.trim());
-let birthday = regexBirthday.test(inputDataEnter[3].value.trim());
-let tournois = regexNumberTournois.test(inputDataEnter[4].value.trim());
- console.log(inputDataEnter[0].id);
+function emailValidation(e){
+    const error = document.querySelector("div.email[data-error]");
+    const email = /^[a-z0-9._-]+@[a-z0-9._-]+.[a-z]{2,20}$/.test(inputDataEnter[2].value.trim());
+    if(email === false){
+        e.preventDefault();
+        error.setAttribute('data-error-visible', 'true');
+        console.log("Le formulaire n'est pas envoyé ! ");
+        
+    }else {
+        error.setAttribute('data-error-visible', 'false');
+        console.log("Le formulaire est envoyé ! ");
+    }
+}   
+
+
+function birthdayValidation(e){
+    const error = document.querySelector("div.birthday[data-error]");
+    const birthday = /^(19[0-9]{2}|2[0-9]{3})-(0[1-9]|1[012])-([123]0|[012][1-9]|31)$/.test(inputDataEnter[3].value.trim());
+    if(birthday === false){
+        e.preventDefault();
+        error.setAttribute('data-error-visible', 'true');
+        console.log("Le formulaire n'est pas envoyé ! ");
+    }else {
+        error.setAttribute('data-error-visible', 'false');
+        console.log("Le formulaire est envoyé ! ");
+    }
+}   
+
+
+function tournoisValidation(e){
+    const error = document.querySelector("div.tournois[data-error]");
+    const tournois = /^[0-9]*$/.test(inputDataEnter[4].value.trim());
+    if(tournois === false){
+        e.preventDefault();
+        error.setAttribute('data-error-visible', 'true');
+        console.log("Le formulaire n'est pas envoyé ! ");
+    }else {
+        error.setAttribute('data-error-visible', 'false');
+        console.log("Le formulaire est envoyé ! ");
+    }
+}  
 
 // On écoute le submit sur le formulaire
 form.addEventListener("submit", (e) => {
-    // On empêche le comportement par défaut pour éviter le rechargement de la page.
-    e.preventDefault();
-    
-
-
-        if((!firstName) && (inputDataEnter[0].id)){
-            console.log("Non valide");
-        }
-
-        if(!last){
-         
-        } 
-         if(!email){
-        
-        }
- 
-
-        
- 
-
-        if(valid){
-            valid &= form.reportValidity();
-            console.log("Votre message a bien été envoyé");
-            // form.reset();
-        }
+    // e.preventDefault();
+    firstNameValidation(e);
+    lastValidation(e);
+    emailValidation(e);
+    birthdayValidation(e);
+    tournoisValidation(e);
 
 });
-
-
-
 
 
